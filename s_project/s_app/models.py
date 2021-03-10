9507a8f3ea45d1db4b632fe3f33e2007d3951649
Babuso  Naik <tejasnaik980@gmail.com>
2021-03-10 20:16:11 +0530

    first

--- s_project/s_app/models.py
+++ s_project/s_app/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+size_choices = (
+      ('small','S'),
+      ('medium','M'),
+      ('large','L'),
+      ('Ex-large','XL')
+
+)
+
+
 class User(models.Model):
     Name=models.CharField(max_length=128)
-    me=models.CharField(max_length=128,blank=True)
     Jersey_Name=models.CharField(max_length=128)
+    jersey_Size=models.CharField(max_length=128,choices=size_choices,default='small')
     # email=models.EmailField(max_length=264,unique=True)
     jersey_Num=models.IntegerField(blank=True, null=True)
-    jersey_Nm=models.IntegerField(blank=True, null=True)
